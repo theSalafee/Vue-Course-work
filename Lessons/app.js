21e623fb83a8304654b27fd58868e47ff1b413b0
Naasir Salafee <naasirbush@icloud.com>
2021-09-19 12:04:59 -0700

    Toggle between hiding and showing

--- Lessons/app.js
+++ Lessons/app.js
@@ -1,16 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
+            showBooks: true,
             title: "Sir",
             author: "Naasir",
             age: 42,
         };
     },
     methods: {
-        changeTitle(title) {
-            this.title = title;
-            console.log(title);
-        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks; // toggles true || false on showBooks
+        }
     },
 });
 app.mount("#app");
