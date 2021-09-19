f70696bbee670bea5a2944bd61ef578f3947c366
Naasir Salafee <naasirbush@icloud.com>
2021-09-19 11:37:48 -0700

    Lesson 10

--- Lessons/app.js
+++ Lessons/app.js
@@ -1,25 +1,16 @@
-
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
-            title: 'Sir',
-            author: 'Naasir',
+            title: "Sir",
+            author: "Naasir",
             age: 42,
-
-        }
+        };
     },
     methods: {
         changeTitle(title) {
-            this.title = title
-            console.log(title)
-        }
-    }
-}) 
-
-
-// You can add methods to the component by adding a 'methods' property
-// The 'this' keyword references the component thereby allowing access to the data props
-
-// Parameters can also be passed from the view to the component
-
-app.mount('#app') 
+            this.title = title;
+            console.log(title);
+        },
+    },
+});
+app.mount("#app");
